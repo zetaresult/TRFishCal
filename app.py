42c74708007f1dd0197de788b8464fc667e7b01a
zetaresult <zetaresult@gmail.com>
2025-08-09 02:29:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -270,8 +270,9 @@ elif menu == "테런 낚싯대 계산기":
 
     st.markdown(f"<div style='font-size: 20px; font-weight: bold; margin-top: 12px;'>한 마리당 약 {(min_default2+max_default2)/2}초이며, 약 {round(rod_seconds/((min_default2+max_default2)/2)):,}개 소모됩니다.</div>", unsafe_allow_html=True)
     st.markdown(f"<div style='font-size: 20px; font-weight: bold; margin-top: 12px;'>보정 전: {selected_name} {round(rod_seconds/((min_default2+max_default2)/2)):,}개는 {round(selected_exp * ((rod_seconds/((min_default2+max_default2)/2)))):,}EXP입니다.</div>", unsafe_allow_html=True)
-    st.markdown(f"<div style='font-size: 20px; font-weight: bold; margin-top: 12px;'>보정 후: {selected_name} {round((rod_seconds/((min_default2+max_default2)/2))*1.5):,}개는 {round(selected_exp * ((rod_seconds/((min_default2+max_default2)/2))*1.5):,}EXP입니다.</div>", unsafe_allow_html=True)
+    st.markdown(f"<div style='font-size: 20px; font-weight: bold; margin-top: 12px;'>보정 후: {selected_name} {round((rod_seconds/((min_default2+max_default2)/2))*1.5):,}개는 {round(selected_exp * ((rod_seconds/((min_default2+max_default2)/2)))*1.5):,}EXP입니다.</div>", unsafe_allow_html=True)
     st.markdown(f"<div style='font-size: 15px; font-weight: bold; margin-top: 12px;'>15~20초 평균 기준으로 계산되었으며, 보정값은 1.5배수입니다.</div>", unsafe_allow_html=True)
+
 
 
 
