cdfea8a43f5b7cca46d41f0ed23d0cf3f5d6e6cc
zetaresult <zetaresult@gmail.com>
2025-09-07 19:48:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -183,10 +183,10 @@ if menu == "경험치 및 낚시 계산기":
     try:
         currentPer = float(currentPer_str)
         if currentPer >= 100:
-                st.warning("값이 100을 초과할 수 없습니다.")
+                st.error("100 미만의 숫자를 입력해주세요.")
                 currentPer = 0.0
         elif currentPer < 0:
-                st.warning("값이 0 미만일 수 없습니다.")
+                st.error("0 이상의 숫자를 입력해주세요.")
                 currentPer = 0.0
     except ValueError:
         st.error("숫자를 입력해주세요.")
@@ -412,6 +412,7 @@ elif menu == "경험치 ↔ 지렁이":
 
     
     
+
 
 
 
