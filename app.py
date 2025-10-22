3295c79c82334339d792c2e00ea71aca254a34be
zetaresult <zetaresult@gmail.com>
2025-10-22 23:41:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ MENU = st.sidebar.radio(
     index = 0 # 기본값
 )
 if MENU == "경험치 및 낚시 계산기":
-    if st.button("[업데이트 내역]"):
+    if st.button("[업데이트 내역 25-10-21]"):
         trU.update_info()
         
     col1, col2 = st.columns([3,1])
@@ -273,6 +273,7 @@ elif MENU == "경험치 ↔ 지렁이":
     else:
         st.info("계산 방식을 하나 선택해주세요.")
     
+
 
 
 
